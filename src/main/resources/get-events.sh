7f61dcd0cb39cb3545f74e8b8bb22fc5d4f4d023
Przemyslaw Ozimkiewicz <p.ozimkiewicz@gmail.com>
2018-06-04 23:27:55 +0200

    initial version of spark processing app and github data loader

---
+++ src/main/resources/get-events.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#./get-events.sh 2017-03 /tmp/input
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters";
+    echo "Usage: ./get-events.sh <YYYY> <MM> /path/to/dest/dir"
+    echo "Example: ./get-events.sh 2016 03 /path/to/dest/dir"
+    exit 1;
+fi
+
+YEAR=$1;
+MONTH=$2;
+DEST=$3
+
+if [ ! -d "$DEST" ]; then
+  echo "Directory $DEST does not exists! Exiting."
+  exit 1;
+fi
+
+
+NODAYS=`date -d "$YEAR-$MONTH-01 +1 month -1 day" "+%d"`;
+START=`date`
+for i in $(seq -f '%02g' 1 $NODAYS)
+do
+   for j in {0..23}
+   do
+      FILENAME="$YEAR-$MONTH-$i-$j.json.gz"
+      URL="http://data.gharchive.org/$FILENAME"
+      #echo $URL
+      wget -P $DEST $URL
+      if [ $? -ne 0 ]; then
+        echo "WARNING: download of $URL failed! Exiting"
+        exit 1;
+      fi
+   done
+done
+STOP=`date`
+
+echo "started $START";
+echo "completed $STOP";
---
+++ src/main/scala/org/poz/spark/DailyGithubApp.scala
@@ -0,0 +1,45 @@
+package org.poz.spark
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import com.databricks.spark.avro._
+
+object DailyGithubApp {
+
+  def main(args: Array[String]) {
+
+
+    val spark = SparkSession
+      .builder
+      .appName("DailyGithubApp")
+      .getOrCreate()
+    import spark.implicits._
+
+    val input_path = spark.sparkContext.getConf.get("spark.ghevents.input")
+    val output_path = spark.sparkContext.getConf.get("spark.ghevents.output")
+    val partitions = spark.sparkContext.getConf.get("spark.ghevents.output.partitions")
+
+    val df = spark.read.json(input_path + "/*.json.gz")
+    val proDF = df.select(
+      $"repo.id".alias("repo_id"),
+      $"repo.name",$"actor.id".alias("user_id"),
+      $"actor.login", $"type", $"payload.action",
+      to_date($"created_at", "yyyy-MM-dd").alias("date"))
+
+    val repositoryAggr = proDF.groupBy("repo_id", "name", "date").agg(
+      count(when($"type" === "WatchEvent" and $"action" === "started", true)).alias("starred"),
+      count(when($"type" === "ForkEvent", true)).alias("forks"),
+      count(when($"type" === "IssuesEvent" and $"action" === "opened", true)).alias("issues"),
+      count(when($"type" === "PullRequestEvent" and $"action" === "opened", true)).alias("pull_requests"))
+
+    repositoryAggr.repartition(partitions.toInt).write.avro(output_path + "/repository")
+
+    val userAggr = proDF.groupBy("user_id", "login", "date").agg(
+      count(when($"type" === "WatchEvent" and $"action" === "started", true)).alias("starred"),
+      count(when($"type" === "IssuesEvent" and $"action" === "opened", true)).alias("issues"),
+      count(when($"type" === "PullRequestEvent" and $"action" === "opened", true)).alias("pull_requests"))
+
+    userAggr.repartition(partitions.toInt).write.avro(output_path + "/user")
+  }
+
+}
